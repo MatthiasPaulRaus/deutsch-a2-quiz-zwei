c2d17551cefc94066dea02e20a7a867699815432
matthias paul raus <81903294+MatthiasPaulRaus@users.noreply.github.com>
2023-08-01 13:45:42 +0200

    second commit

---
+++ src/QuestionsList.jsx
@@ -0,0 +1,221 @@
+const QuestionsList = [
+    {
+        question:"Was haben Sie nach dem Studium gemacht?",
+        answersList: [
+            {answer:"Da habe ich eine lange Reise gemacht.", isCorrect:true},
+            {answer:"Zuerst habe ich das Abitur gemacht.", isCorrect:false},
+            {answer:"Ich wollte ein Praktikum machen.", isCorrect:false},
+        ]
+    },{
+        question:"Wer sind die Leute auf dem Foto?",
+        answersList: [
+            {answer:"Das war ein Ausflug im Sommer.", isCorrect:false},
+            {answer:"Das waren unsere Großeltern.", isCorrect:true},
+            {answer:"Das Dorf, in dem meine Großeltern lebten.", isCorrect:false},
+        ]
+    },{
+        question:"Was meinst du? Wohin soll ich den Teppich legen?",
+        answersList: [
+            {answer:"Der Flur sieht gut aus.", isCorrect:false},
+            {answer:"Im Flur war noch Platz.", isCorrect:false},
+            {answer:"Leg ihn doch in den Flur.", isCorrect:true},
+        ]
+    },{
+        question:"Unser Bundesland liegt im Südwesten Deutschlands",
+        answersList: [
+            {answer:"und grenzt an zwei europäische Länder.", isCorrect:true},
+            {answer:"und hat keine Grenze zu Österreich.", isCorrect:false},
+            {answer:"und hat eine Grenze zu Liechtenstein.", isCorrect:false},
+        ]
+    },{
+        question:"Bezahlen Sie zusammen oder getrennt?",
+        answersList: [
+            {answer:"Gemeinsam, bitte.", isCorrect:false},
+            {answer:"Ich bezahle alles zusammen.", isCorrect:true},
+            {answer:"Am liebsten mit Kreditkarte.", isCorrect:false},
+        ]
+    },{
+        question:"Muss ich wirklich auf den Kirchturm steigen?",
+        answersList: [
+            {answer:"Leider gibt es keinen Aufzug.", isCorrect:false},
+            {answer:"Das darfst du nicht verpassen.", isCorrect:false},
+            {answer:"Ja, die Aussicht von da oben ist super.", isCorrect:true},
+        ]
+    },{
+        question:"Welches Fest magst du am liebsten?",
+        answersList: [
+            {answer:"Das Oktoberfest in München.", isCorrect:true},
+            {answer:"Das im Oktober ist mir lieber.", isCorrect:false},
+            {answer:"Das Sommerfest mag ich sehr.", isCorrect:false},
+        ]
+    },{
+        question:"Sie hätten schon früher kommen sollen.",
+        answersList: [
+            {answer:"Wir haben nur eine Tagessuppe.", isCorrect:false},
+            {answer:"Jetzt haben wir keine Tagessuppe mehr.", isCorrect:true},
+            {answer:"Die Tagessuppe kann ich Ihnen empfehlen.", isCorrect:false},
+        ]
+    },{
+        question:"Wie lernen Sie die neuen Wörter?",
+        answersList: [
+            {answer:"Ich höre gern Lieder und singe mit.", isCorrect:false},
+            {answer:"Ich muss sie oft hören und laut aussprechen.", isCorrect:false},
+            {answer:"Ich lerne die Regeln und schreibe sie auf.", isCorrect:true},
+        ]
+    },{
+        question:"Für einen guten Kartoffelsalat braucht man:",
+        answersList: [
+            {answer:"Kartoffeln, Zwiebeln, Essiggurken.", isCorrect:true},
+            {answer:"Eier, Kartoffeln, Zwiebeln.", isCorrect:false},
+            {answer:"Essiggurken, Oliven und Eier.", isCorrect:false},
+        ]
+    },
+    
+    {
+        question:"Seitdem ich mit dem Fahrrad zur Arbeit fahre,",
+        answersList: [
+            {answer:"fühle ich mich viel gesünder.", isCorrect:true},
+            {answer:"bin ich gesund.", isCorrect:false},
+            {answer:"trainiere ich meine Gesundheit.", isCorrect:false},
+        ]
+    },{
+        question:"Bis ich Lehrerin geworden bin,",
+        answersList: [
+            {answer:"machte ich kein Studium.", isCorrect:false},
+            {answer:"musste ich lange studieren.", isCorrect:true},
+            {answer:"wollte ich Kinder unterrichten.", isCorrect:false},
+        ]
+    },{
+        question:"Seitdem ich keinen Kaffee mehr trinke,",
+        answersList: [
+            {answer:"schlafe ich nur noch ganz wenig.", isCorrect:false},
+            {answer:"habe ich immer wieder Kopfschmerzen.", isCorrect:false},
+            {answer:"bin ich viel seltener nervös.", isCorrect:true},
+        ]
+    },{
+        question:"Was machst du mit dem alten Motorrad?",
+        answersList: [
+            {answer:"Ich lasse es in der Werkstatt reparieren.", isCorrect:true},
+            {answer:"Beim Kundendienst kann ich es reparieren.", isCorrect:false},
+            {answer:"Der Autoverkäufer will es reparieren.", isCorrect:false},
+        ]
+    },{
+        question:"Von wem lässt du dir die Wohnung streichen?",
+        answersList: [
+            {answer:"Das kann ich auch alleine nicht machen.", isCorrect:false},
+            {answer:"Das mache ich nicht selber, sondern der Maler.", isCorrect:true},
+            {answer:"Ein Maler kommt und bringt die Farben.", isCorrect:false},
+        ]
+    },{
+        question:"Gibt es Zeugen für den Unfall?",
+        answersList: [
+            {answer:"Wir haben sofort die Polizei angerufen.", isCorrect:false},
+            {answer:"Ein Auto ist gegen das Verkehrsschild gefahren.", isCorrect:false},
+            {answer:"Ja, die Verkäuferin aus dem Geschäft hat alles gesehen.", isCorrect:true},
+        ]
+    },{
+        question:"Worüber haben Sie mit dem Architekten gesprochen?",
+        answersList: [
+            {answer:"Darüber, wie groß der Keller werden soll.", isCorrect:true},
+            {answer:"Über den Heizungstechniker.", isCorrect:false},
+            {answer:"Im ersten Stock fehlt ein Fenster.", isCorrect:false},
+        ]
+    },{
+        question:"Schon als Kind war ich sehr intelligent.",
+        answersList: [
+            {answer:"Ich konnte schon mit drei Jahren laufen.", isCorrect:false},
+            {answer:"Ich habe schon mit vier Jahren Schach gespielt.", isCorrect:true},
+            {answer:"Ich durfe schon mit fünf Jahren in den Zoo gehen.", isCorrect:false},
+        ]
+    },{
+        question:"Ich sollte Sie gestern anrufen.",
+        answersList: [
+            {answer:"Was wollen Sie mich denn fragen?", isCorrect:false},
+            {answer:"Leider haben Sie vergessen, wo wir uns treffen wollten.", isCorrect:false},
+            {answer:"Tut mir leid, dass ich es nicht mehr geschafft habe.", isCorrect:true},
+        ]
+    },{
+        question:"Mochten Sie als Kind auch kein Gemüse essen?",
+        answersList: [
+            {answer:"Ja, Blumenkohl wollte ich nie essen.", isCorrect:true},
+            {answer:"Nein, ich durfte keine Nüsse essen.", isCorrect:false},
+            {answer:"Schon als Kind hatte ich eine Allergie gegen Birnen.", isCorrect:false},
+        ]
+    },
+    
+
+    {
+        question:"Bitte, wo ist der Fitnessraum?",
+        answersList: [
+            {answer:"Den finden Sie unten im Keller.", isCorrect:true},
+            {answer:"Der Fitnessraum ist unter dem Keller.", isCorrect:false},
+            {answer:"Neben dem Keller gibt es einen.", isCorrect:false},
+        ]
+    },{
+        question:"Wo wird der Plastikmüll gesammelt?",
+        answersList: [
+            {answer:"Wir machen hier keinen Müll.", isCorrect:false},
+            {answer:"Auf der Straße stehen mehrere Container.", isCorrect:true},
+            {answer:"Der Müllcontainer ist schon voll.", isCorrect:false},
+        ]
+    },{
+        question:"Wir haben eine Überraschungsparty für die neue Kollegin organisiert.",
+        answersList: [
+            {answer:"Wir hoffen, dass Sie sich darauf freuen.", isCorrect:false},
+            {answer:"Eine Kollegin sollte man nicht überraschen.", isCorrect:false},
+            {answer:"Hoffentlich freut sie sich darüber.", isCorrect:true},
+        ]
+    },{
+        question:"Wie oft haben Sie schon Ihr Handy vergessen?",
+        answersList: [
+            {answer:"Leider ist mir das schon viermal passiert.", isCorrect:true},
+            {answer:"Es passiert mir leider immer wieder.", isCorrect:false},
+            {answer:"Heute kann mir das nicht mehr passieren.", isCorrect:false},
+        ]
+    },{
+        question:"Wie wird die Party vorbereitet?",
+        answersList: [
+            {answer:"Keine Ahnung, was da gefeiert wird.", isCorrect:false},
+            {answer:"Wir werden zusammen eine Überraschung vorbereiten.", isCorrect:false},
+            {answer:"Wir machen alles gemeinsam. Jeder hilft mit.", isCorrect:true},
+        ]
+    },{
+        question:"Ich würde gern wissen,",
+        answersList: [
+            {answer:"ob das Schwimmbad schon geöffnet ist.", isCorrect:true},
+            {answer:"wie die Disko geöffnet wird.", isCorrect:false},
+            {answer:"wo Sie das Café finden.", isCorrect:false},
+        ]
+    },{
+        question:"Woher kommst du denn so spät?",
+        answersList: [
+            {answer:"Beim Hausarzt konnte ich lange warten.", isCorrect:false},
+            {answer:"Ich war gerade noch beim Arzt. Da musste ich warten.", isCorrect:true},
+            {answer:"Auf den Termin beim Hausarzt habe ich lange gewartet.", isCorrect:false},
+        ]
+    },{
+        question:"Wollen wir am Wochenende etwas gemeinsam unternehmen?",
+        answersList: [
+            {answer:"Das ist doch bestimmt ganz langweilig.", isCorrect:false},
+            {answer:"Ja gern, mit wem willst du den Ausflug machen?", isCorrect:false},
+            {answer:"Einverstanden! Hast du schon einen Vorschlag?", isCorrect:true},
+        ]
+    },{
+        question:"Am Wochenende langweile ich mich oft. Hast du einen Tipp?",
+        answersList: [
+            {answer:"Versuch es doch mal mit einem spannenden Roman.", isCorrect:true},
+            {answer:"Na ja, wenn du kein Interesse daran hast.", isCorrect:false},
+            {answer:"Warum denn nur am Wochenende?", isCorrect:false},
+        ]
+    },{
+        question:"Was machst du oft am Wochenende?",
+        answersList: [
+            {answer:"Am Samstag arbeite ich nicht, dann habe ich Freizeit.", isCorrect:false},
+            {answer:"Ich war auf einem Konzert. Das war fantastisch.", isCorrect:false},
+            {answer:"Ich besuche sehr gern Ausstellungen.", isCorrect:true},
+        ]
+    },
+
+].sort(() => Math.random() - 0.5);
+
+export default QuestionsList;
